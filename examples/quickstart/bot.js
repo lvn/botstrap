1435d5932086bdfdfde720633bbe8af4b4ed7c24
Elvin Yung <elvinyung1@gmail.com>
2015-07-05 01:06:30 -0700

    Actual working example bot

--- examples/quickstart/bot.js
+++ examples/quickstart/bot.js
@@ -1,23 +1,18 @@
 var botstrap = require('../../lib');
 
 var bot = botstrap.createBot({
-  token: 'TOKEN_HERE'
-});
-//
-// // responds to any channel_joined events
-// bot.on('channel_joined', function(channel, response) {
-//   response.write('Hello' + channel.name + '!');
-// });
-
-// responds to any message beginning with !random
-bot.command('random', function reverse(message, response) {
-  response.write(Math.random());
+  token:
 });
 
 // responds to any message beginning with !reverse or !esrever
 bot.command(['reverse', 'esrever'], function reverse(message, response) {
   var payload = message.argv[1];
-  response.write(payload.split('').reverse().join(''));
+  payload && response.write(payload.split('').reverse().join(''));
+});
+
+// responds to any message beginning with !random
+bot.command('random', function random(message, response) {
+  response.write(Math.random());
 });
 
 // responds to any message
@@ -26,10 +21,5 @@ bot.onMessage(function(message, response) {
     response.write('pong');
   }
 });
-//
-// // responds to any message of subtype channel_join
-// bot.onMessage('channel_join', function(message, response) {
-//   response.write('Welcome ' + message.user.name + '!');
-// });
 
 bot.start();
