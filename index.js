f65c93b109e2005bea1daaac1494a93f6fc5205b
Elvin Yung <elvin@elvinyung.com>
2015-11-01 16:49:44 -0500

    Use path.resolve to resolve path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env iojs
 
-var program = require('commander'),
+var path = require('path'),
+  program = require('commander'),
   fs = require('fs'),
   sequoria = require('sequoria'),
   createBot = require('./lib').createBot;
@@ -11,14 +12,7 @@ function main() {
     .usage('[configFile]')
     .action(function(configFile){
       // this module is being directly run.
-      var configFile = configFile || 'config.json';
-      if (!configFile.startsWith('/')) {
-        // hack: should use proper path joining package
-        configFile = [
-          process.cwd(),
-          configFile
-        ].join('/');
-      }
+      var configFile = path.resolve(configFile || 'config.json');
 
       var config = JSON.parse(fs.readFileSync(configFile));
       var bot = createBot(config);
