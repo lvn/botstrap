121380a73254d8a76165bb0cc6f3ce48fb44bf88
Elvin Yung <elvin@elvinyung.com>
2015-12-23 11:22:23 -0500

    Move main executable to bin folder

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-#!/usr/bin/env node
+'use strict';
 
-var path = require('path'),
-  program = require('commander'),
-  fs = require('fs'),
-  sequoria = require('sequoria'),
-  createBot = require('./lib').createBot;
-
-function main() {
-  program
-    .version(require('./package').version)
-    .usage('[configFile]')
-    .action(function(configFile){
-      // this module is being directly run.
-      var configFile = path.resolve(configFile || 'config.json');
-
-      var config = JSON.parse(fs.readFileSync(configFile));
-      var bot = createBot(config);
-      bot.start();
-    })
-    .parse(process.argv);
-}
-
-if (require.main === module) {
-  main();
-}
-
-module.exports = createBot;
+module.exports = require('./lib');
